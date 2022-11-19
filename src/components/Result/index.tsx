d993a8be3edd49959cfcef19d44bc8d520937ece
carlos.santos <carlos.santos@neomind.com.br>
2022-11-18 23:53:41 -0300

    feature: result template

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,5 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { GlobalContext } from 'src/Context';
 
-export const Result: FC<any> = () => {
-  return <>{'00:00'}</>;
+import { ResultContainer } from './styles';
+
+export const Result: FC = () => {
+  const ctx = useContext(GlobalContext);
+
+  return <ResultContainer>
+    {/* Regular Work */}
+    <div>{'Regular: 07: 00 | 08:45 | -01:45'}</div>
+    {/* Extra Work */}
+    <div>{'Extra: 00:00 | 02:00 | -03:45'}</div>
+    {/* Interval */}
+    <div>{'01:00'}</div>
+  </ResultContainer>;
 };
